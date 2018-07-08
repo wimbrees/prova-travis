ce9ad998b8f85c59f0efb5f84b0ee91342326145
wimbrees <wimbrees@users.noreply.github.com>
2018-07-08 21:02:30 +0200

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel test
+                    Laravel
                 </div>
 
                 <div class="links">
